70b5bc76a9d31209e9880058f79e21da9f737c4f
현유진 <gusdbwls9772@gmail.com>
2021-03-12 19:08:24 +0900

    Study SCSS

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Start from './Start';
-import './style.css';
+import BucketList from './BucketList';
+// import Start from './Start';
+// import './style.css';
+import './scss_ex.scss';
 
 // 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
@@ -9,14 +11,25 @@ class App extends React.Component {
     
     // App 컴포넌트의 state를 정의한다.
     this.state = {
-      name: "미더덕"
+      name: "미더덕",
+      list: ['자가용 장만하기', '스카이 다이빙', '인생 최종목표 알지?']
     };
   }
 
   render() {
     return (
+      // <div className="App">
+      //   <Start name={this.state.name} />
+      // </div>
+
       <div className="App">
-        <Start name={this.state.name} />
+        <div className="container">
+            <h1 className="title">내 버킷리스트</h1>
+            <hr className="line"/>
+            {/* 컴포넌트를 넣어줍니다. */}
+            {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
+            <BucketList list={this.state.list} />
+        </div>
       </div>
     )
   }
