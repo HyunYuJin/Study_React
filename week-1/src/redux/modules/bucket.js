9d3f6c52adc4ad3a23e037ec7d42185d31a3044f
현유진 <gusdbwls9772@gmail.com>
2021-03-31 02:56:37 +0900

    Use redux-thunk for firestore (#4)
    Check for redux-thunk middleware installation and use

--- week-1/src/redux/modules/bucket.js
+++ week-1/src/redux/modules/bucket.js
@@ -1,5 +1,10 @@
 // bucket.js
 
+// firestore 객체 가져오기
+import { firestore } from '../../firebase';
+
+const bucket_db = firestore.collection("bucket");
+
 // Actions
 const LOAD = "bucket/LOAD";
 const CREATE = "bucket/CREATE";
@@ -15,41 +20,105 @@ const initialState = {
 }
 
 // Action Creators
-export const loadBucket = () => {
-    return { type: LOAD };
+export const loadBucket = (bucket) => {
+    return { type: LOAD, bucket };
 }
 
 export const createBucket = (bucket) => { // input에서 입력한 새로운 text는 bucket으로 넘어온다.
     return { type: CREATE, bucket };
 }
 
+export const updateBucket = (bucket_index) => {
+    return { type: UPDATE, bucket_index }
+}
+
 export const deleteBucket = (bucket_index) => {
     return { type: DELETE, bucket_index }
 }
 
-export const updateBucket = (bucket_index) => {
-    return { type: UPDATE, bucket_index }
+
+// 파이어베이스랑 통신하는 부분
+export const loadBucketFB = () => {
+    return function (dispatch) {
+      
+        bucket_db.get().then((docs) => {
+            let bucket_data = [];
+            docs.forEach((doc) => {
+                if (doc.exists) {
+                    bucket_data = [...bucket_data, {id: doc.id, ...doc.data()}];
+                }
+            });
+  
+            // 이제 액션 생성 함수한테 가져온 데이터를 넘겨준당
+            dispatch(loadBucket(bucket_data));
+        });
+    };
+};
+
+export const addBucketFB = (bucket) => {
+    return function (dispatch) {
+        let bucket_data = { text: bucket, completed: false };
+
+        bucket_db.add(bucket_data).then((docRef) => {
+            bucket_data = { ...bucket_data, id: docRef.id };
+
+            dispatch(createBucket(bucket_data));
+        });
+    }
+}
+
+export const updateBucketFB = (index) => {
+    return function (dispatch, getState) {
+        // store에 저장된 state 정보들을 가지고 온다.
+        // bucket 안에 list라는 state의 index 값을 가지고 온다.
+        let _bucket_data = getState().bucket.list[index];
+
+        let bucket_data = { ..._bucket_data, completed: true };
+        if (!bucket_data.id) {
+            return;
+        }
+
+        bucket_db.doc(_bucket_data.id).update(bucket_data).then((docRef) => {
+            dispatch(updateBucket(index));
+        }).catch((error) => {
+            console.log("updateBucketFB: ", error);
+        });
+    }
+}
+
+export const deleteBucketFB = (index) => {
+    return function (dispatch, getState) {
+        let _bucket_data = getState().bucket.list[index];
+
+        if (!_bucket_data.id) {
+            return;
+        }
+
+        bucket_db.doc(_bucket_data.id).delete().then((docRef) => {
+            dispatch(deleteBucket(index));
+        }).catch((error) => {
+            console.log("deleteBucketFB: ", error);
+        });
+    }
 }
 
 // Reducer
 export default function reducer(state = initialState, action = {}) {
     switch (action.type) {
-        case "bucket/LOAD":
+        case "bucket/LOAD": {
+            if (action.bucket.length > 0) {
+                return { list: action.bucket };
+            }
+        
             return state;
+        }
 
-        case "bucket/CREATE":
-            const new_bucket_list = [...state.list, { text: action.bucket, completed: false }];
+        case "bucket/CREATE": {
+            const new_bucket_list = [...state.list, action.bucket];
             return { list: new_bucket_list };
+        }
 
-        case "bucket/DELETE":
-            const bucket_list = state.list.filter((value, index) => {
-                if (index !== action.bucket_index) {
-                    return value;
-                }
-            });
-            return { list: bucket_list }
-
-        case "bucket/UPDATE":
+        case "bucket/UPDATE": {
             const bucket_update = state.list.map((value, index) => {
                 if (index === action.bucket_index) {
                     return { ... value, completed: true };
@@ -58,6 +127,16 @@ export default function reducer(state = initialState, action = {}) {
                 }
             });
             return { list: bucket_update };
+        }
+
+        case "bucket/DELETE": {
+            const bucket_list = state.list.filter((value, index) => {
+                if (index !== action.bucket_index) {
+                    return value;
+                }
+            });
+            return { list: bucket_list }
+        }
 
         default:
             return state;
